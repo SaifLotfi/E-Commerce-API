fda9e3fec403bb388d0518cf401484ec14f6f19a
Saif <swaifweaa@gmail.com>
2023-09-07 11:16:58 +0300

    added jwt

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,93 +1,105 @@
-import User from "../models/user.js";
-import bcrypt from "bcryptjs";
+import User from '../models/user.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import {
-  UnauthenticatedError,
-  NotFoundError,
-  BadRequestError,
-} from "../errors/index.js";
+    UnauthenticatedError,
+    NotFoundError,
+    BadRequestError,
+} from '../errors/index.js';
 // @desc    Auth user & get token
 // @route   POST /api/users/login
 // @access  Public
 const authUser = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
-  if (!user || !(await user.matchPassword(password.toString()))) {
-    throw new UnauthenticatedError(`Invalid Email Or Password!`);
-  }
+    if (!user || !(await user.matchPassword(password.toString()))) {
+        throw new UnauthenticatedError(`Invalid Email Or Password!`);
+    }
 
-  res.json({
-    _id: user._id,
-    name: user.name,
-    email: user.email,
-    isAdmin: user.isAdmin,
-  });
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    });
+
+    res.cookie('jwt', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development',
+        sameSite: 'strict',
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
+
+    res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+    });
 };
 
 // @desc    delete token
 // @route   POST /api/users/logout
 // @access  Private
 const logoutUser = async (req, res) => {
-  res.send("logout user");
+    res.send('logout user');
 };
 
 // @desc    Register a new user
 // @route   POST /api/users
 // @access  Public
 const registerUser = async (req, res) => {
-  res.send("register user");
+    res.send('register user');
 };
 
 // @desc    Get user profile
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = async (req, res) => {
-  res.send("get user profile");
+    res.send('get user profile');
 };
 
 // @desc    Update user profile
 // @route   PUT /api/users/profile
 // @access  Private
 const updateUserProfile = async (req, res) => {
-  res.send("update user profile");
+    res.send('update user profile');
 };
 
 // @desc    Get all users
 // @route   GET /api/users
 // @access  Private/Admin
 const getUsers = async (req, res) => {
-  res.send("get users");
+    res.send('get users');
 };
 
 // @desc    Delete user
 // @route   DELETE /api/users/:id
 // @access  Private/Admin
 const deleteUser = async (req, res) => {
-  res.send("delete user");
+    res.send('delete user');
 };
 
 // @desc    Get user by ID
 // @route   GET /api/users/:id
 // @access  Private/Admin
 const getUserById = async (req, res) => {
-  res.send("get user by id");
+    res.send('get user by id');
 };
 
 // @desc    Update user
 // @route   PUT /api/users/:id
 // @access  Private/Admin
 const updateUser = async (req, res) => {
-  res.send("update user");
+    res.send('update user');
 };
 
 export {
-  authUser,
-  logoutUser,
-  registerUser,
-  getUserProfile,
-  updateUserProfile,
-  getUsers,
-  deleteUser,
-  getUserById,
-  updateUser,
+    authUser,
+    logoutUser,
+    registerUser,
+    getUserProfile,
+    updateUserProfile,
+    getUsers,
+    deleteUser,
+    getUserById,
+    updateUser,
 };
