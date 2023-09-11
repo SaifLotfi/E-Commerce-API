d09609b3b4c2f5483d9247dc1224f03f67716570
Saif <swaifweaa@gmail.com>
2023-09-11 08:21:54 +0300

    added delete product logic

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -6,7 +6,7 @@ import {
 } from '../errors/index.js';
 
 //@desc     Fetch all Products
-//route     Get /api/products
+//route     Get /api/products/:id
 //@access   Public
 const getProduct = async (req, res, next) => {
     const productId = req.params.id;
@@ -44,7 +44,7 @@ const createProduct = async (req, res, next) => {
     });
     await product.save();
     res.status(201).json(product);
-}
+};
 //@desc     Edit a Product
 //route     PUT /api/products/:id
 //@access   Private/Admin
@@ -52,13 +52,13 @@ const updateProduct = async (req, res, next) => {
     const productId = req.params.id;
     const product = await Product.findById(productId);
     if (product) {
-        product.name = req.body.name||product.name;
-        product.price = req.body.price||product.price;
-        product.image = req.body.image||product.image;
-        product.brand = req.body.brand||product.brand;
-        product.category = req.body.category||product.category;
-        product.countInStock = req.body.countInStock||product.countInStock;
-        product.description = req.body.description||product.description;
+        product.name = req.body.name || product.name;
+        product.price = req.body.price || product.price;
+        product.image = req.body.image || product.image;
+        product.brand = req.body.brand || product.brand;
+        product.category = req.body.category || product.category;
+        product.countInStock = req.body.countInStock || product.countInStock;
+        product.description = req.body.description || product.description;
 
         const updatedProduct = await product.save();
         return res.status(200).json(updatedProduct);
@@ -67,4 +67,19 @@ const updateProduct = async (req, res, next) => {
     throw new NotFoundError(`No Such Product with Id :${productId}`);
 };
 
-export { getProduct, getAllProducts,createProduct ,updateProduct};
+//@desc     Delete a Product
+//route     DELETE /api/products/:id
+//@access   Private/Admin
+const deleteProduct = async (req, res, next) => {
+    const productId = req.params.id;
+    const product = await Product.findById(productId);
+    if (product) {
+        await Product.deleteOne({ _id: productId });
+        return res
+            .status(200)
+            .json({ message: 'Product Deleted Successfully' });
+    }
+    throw new NotFoundError(`No Such Product with Id :${productId}`);
+};
+
+export { getProduct, getAllProducts, createProduct, updateProduct,deleteProduct };
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,11 +1,21 @@
 import express from 'express';
 import Product from '../models/product.js';
-import { getProduct,getAllProducts,createProduct, updateProduct } from '../controllers/product.js';
+import {
+    getProduct,
+    getAllProducts,
+    createProduct,
+    updateProduct,
+    deleteProduct,
+} from '../controllers/product.js';
 import { isAdmin, isAuth } from '../middleware/authentication.js';
 const router = express.Router();
 
-router.route('/').get(getAllProducts).post(isAuth,isAdmin,createProduct);
+router.route('/').get(getAllProducts).post(isAuth, isAdmin, createProduct);
 
-router.route('/:id').get(getProduct).put(isAuth,isAdmin,updateProduct);
+router
+    .route('/:id')
+    .get(getProduct)
+    .put(isAuth, isAdmin, updateProduct)
+    .delete(isAuth, isAdmin, deleteProduct);
 
 export default router;
